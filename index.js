5330ed9d926f5d49663f0e10cf41359c085a2359
dankiprop <dankiproh@gmail.com>
2022-08-22 15:25:44 +0300

    Done with assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+const c = 2000
+const d = 100
+
+function divide(a, b) {
+    if ("c/d") {
+       return 20 
+    }
+};
+const x=2
+const square = (x) => {
+    return x * x
+}
+
+const a = 3
+const b = 4
+const add = (a, b) => {
+    return a + b
+}
